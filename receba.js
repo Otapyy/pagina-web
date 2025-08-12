777ab99a52b43e849044859604e6a1bc277d0595
Daniel Stamm P. O <dani.stamm.pessoa.oliveira@gmail.com>
2025-08-12 11:16:02 -0300

    quarta

--- receba.js
+++ receba.js
@@ -1,9 +1,41 @@
-titleABC = document.getElementById("1");
-buttonAlterar = document.getElementById("2");
+const titleABC = document.getElementById("1");
+const b = document.getElementById("2");
+const tfValue = document.getElementById("tfValue");
+const btnConfirm = document.getElementById("buttonConfirm")
+let controler = false;
+let valorDado = [];
+let indice = 0;
 
 function alterarTitle() {
-    titleABC.innerHTML = "DEF";
+
+    if (controler == false){
+        titleABC.innerHTML = "DEF";
+        controler = true;
+
+    } else{
+        titleABC.innerHTML = "ABC";
+        controler = false;
+
+    }
+}
+
+function addValueToArray(){
+
+        valorDado[indice] = [tfValue.value];
+        indice++;
+    
 }
 
 b.addEventListener("click", alterarTitle);
 
+btnConfirm.addEventListener("click", addValueToArray);
+
+for(let i = 0; i >= 3; i++){
+    console.log(valorDado[i]);
+}
+
+if(indice == 2){
+console.log(valorDado[0]);
+console.log(valorDado[1]);
+console.log(valorDado[2]);
+}
